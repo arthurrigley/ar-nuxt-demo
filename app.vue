70b6d64d95b1d89255faa21a4c8573ece2ecb08a
Arthur Rigley <arthur.rigley@vtuk.com>
2024-11-18 10:59:45 +0000

    Init

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <Footer />
+</template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.1s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.25rem);
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'iam // frontend guild',
+      templateParams: {
+        titleTemplate: '%s %separator %siteName',
+        separator: '//',
+      },
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-icon'],
+  css: ['~/assets/css/main.css'],
+  routeRules: {
+    '/': { prerender: true },
+  },
+  components: [
+    { path: '~/components/', pathPrefix: false },
+    { path: '~/components/layouts', pathPrefix: true },
+  ],
+  compatibilityDate: '2024-11-12',
+})
